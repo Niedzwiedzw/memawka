72423c3e2e0d07734f44e5dffd34d6b0387dfa7c
niedzwiedzwo <wojciech.brozek@dlabs.pl>
2017-11-29 19:47:23 +0100

    access token file maintenence

--- koparka_memow/settings/settings.py
+++ koparka_memow/settings/settings.py
@@ -1,5 +1,15 @@
-with open('access_token.txt') as f:
-    accessToken = f.read()
+import os
+try:
+    with open('access_token.txt', 'r') as f:
+        accessToken = f.read()
+except FileNotFoundError as e:
+    with open('access_token.txt', 'w') as f:
+        token = input("No access token. You can type it here or press [ENTER] to add it manually into\n"
+                      f"{os.path.join(os.getcwd(), 'access_token.txt')}\n\n>>")
+        if token:
+            f.write(token)
+        else:
+            exit(0)
 
 SETTINGS = {
     'groupID': 987425344635130,
